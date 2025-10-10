1ece3934f496964a84b21a3549793b413e9caf6e
HelloSirJay <hellosirjordan@gmail.com>
2025-10-10 14:49:48 -0700

    Implement tasker CLI with initial command handling and update package.json

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+#! /usr/bin/env node
+const readline = require('node:readline')
+const { stdin: input, stdout: output } = require('node:process')
+
+const rl = readline.createInterface({ input, output })
+
+rl.question('Wellcome to tasker! What would you like to do:\n', input => {
+  if (input.match(/(-h)/i)) {
+    console.log('help...')
+    rl.close()
+  } else if (input.match(/^c(reate)/gi)) {
+    console.log('Create...')
+    rl.close()
+  } else if (input.match(/^a(dd)/i)) {
+    console.log('Add...')
+    rl.close()
+  } else {
+    console.error('Invalid')
+    rl.close()
+  }
+})
