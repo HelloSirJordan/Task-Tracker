c2209339e30c380bc5d2b1dc819b5ec74dfec166
HelloSirJay <hellosirjordan@gmail.com>
2025-10-12 19:25:55 -0700

    feat: implement create (add) functionality for single and multiple tasks

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 #! /usr/bin/env node
 const readline = require('node:readline')
-const { stdin: input, stdout: output } = require('node:process')
+const fs = require('node:fs')
 
+const { stdin: input, stdout: output } = require('node:process')
+const { json } = require('node:stream/consumers')
 const rl = readline.createInterface({ input, output })
+
+let tasks = []
+class Task {
+  constructor () {
+    this.id = 0
+    this.description = ''
+    this.status = 'to-do' //enum to-do, inprogress, done, canceled, incomplete
+    this.completedAt = ''
+    this.createdAt = new Date()
+    this.updatedAt = ''
+  }
+
+  updateStatus (status) {
+    this.status = status
+    this.updatedAt = new Date()
+    if (status === 'done') {
+      this.completedAt = new Date()
+    } else {
+      this.completedAt = ''
+    }
+  }
+}
 
 rl.question('Wellcome to tasker! What would you like to do:\n', input => {
   if (input.match(/(-h)/i)) {
@@ -12,8 +36,70 @@ rl.question('Wellcome to tasker! What would you like to do:\n', input => {
     console.log('Create...')
     rl.close()
   } else if (input.match(/^a(dd)/i)) {
-    console.log('Add...')
+    let task = new Task()
+    task.description = input.replace(/^a(dd)/, '').trim()
+    fs.readFile('./tasks.json', 'utf8', (err, jsonString) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          task.id++
+
+          tasks.push(task)
+          fs.writeFileSync(
+            './tasks.json',
+            JSON.stringify(tasks, null, 2),
+            err => {
+              if (err) {
+                console.error(err)
+              }
+            }
+          )
+          console.log(`Task added successfully (ID: ${task.id})`)
+        } else {
+          console.log('Unknown error')
+        }
+      } else {
+        let data = JSON.parse(jsonString)
+
+        task.id = data.length + 1
+
+        data.push(task)
+
+        fs.writeFile('./tasks.json', JSON.stringify(data, null, 2), err => {
+          if (err) {
+            console.error(err)
+          }
+        })
+        console.log(`Task added successfully (ID: ${task.id})`)
+      }
+    })
+
     rl.close()
+  } else if (input.match(/^l(ist)/i)) {
+    fs.readFile('./tasks.json', 'utf-8', (err, jsonString) => {
+      if (err) {
+        console.error(err)
+      }
+      try {
+        const data = JSON.parse(jsonString)
+        console.log(data)
+      } catch (error) {
+        console.error(error)
+      }
+    })
+  } else if (input.match(/^u(pdate)/i)) {
+    fs.readFile('./tasks.json', 'utf-8', (err, jsonString) => {
+      if (err) {
+        console.error(err)
+      }
+      try {
+        let data = JSON.parse(jsonString)
+        //data.task.updatedAt = new Date()
+      } catch (error) {
+        console.error(error)
+      }
+    })
+  } else if (input.match(/^t(est)/)) {
+    console.log(typeof input)
   } else {
     console.error('Invalid')
     rl.close()
